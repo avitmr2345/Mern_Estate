8dd3e6ef2204fea76e0fdbc696809310bc699da1
Avish <avitmr2345@gmail.com>
2024-04-07 17:45:36 +0530

    created Signin api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,34 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-    const {username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({username, email, password: hashedPassword });
-    try{
-        await newUser.save();
-        res.status(201).json("User created successfully.")
-    }
-    catch (error){
-        next(error);        //it is using the middleware of index.js
-    }
-} 
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10); //it hashes the password
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully.");
+  } catch (error) {
+    next(error); //it is using the middleware of index.js
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email }); //it checks if the user email is present in DB or not
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password); //it compares the provided password and the stored password in the DB.
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc; //it only sends the data to browser without sending the password
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
